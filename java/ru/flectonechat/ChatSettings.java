2fdcd1258716499640228c23ffaa362dd3449a6a
TheFaser <69783345+TheFaser@users.noreply.github.com>
2022-02-08 23:40:59 +0500

    test

--- java/ru/flectonechat/ChatSettings.java
+++ java/ru/flectonechat/ChatSettings.java
@@ -27,10 +27,6 @@ public class ChatSettings implements Listener {
 
         removeRecipientIgnore(event);
 
-        if(enableGlobalChat){
-            globalChat(event);
-        }
-
         localChat(event);
         event.getRecipients().clear();
 
