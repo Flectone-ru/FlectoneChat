8adb0df31799269fab1af1677f69bd897659b028
TheFaser <69783345+TheFaser@users.noreply.github.com>
2022-02-08 23:52:52 +0500

    Last test

--- src/main/java/ru/flectonechat/Commands/Actions.java
+++ src/main/java/ru/flectonechat/Commands/Actions.java
@@ -58,7 +58,8 @@ public class Actions implements CommandExecutor {
 
             flectonePlayer.setActionsInventory(actionsInventory);
         }
-        
+
+        eventPlayer.openInventory(actionsInventory);
         FlectonePlayer flectoneEventPlayer = plugin.allPlayers.get(eventPlayerName);
         flectoneEventPlayer.setLastClickedInventory(actionsInventory, clickedPlayer);
 
